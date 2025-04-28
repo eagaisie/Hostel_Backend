31bf9a19a7d437ffdd756798e63f9e706b187348
eagaisie <emmanuel.gaisie@acity.edu.gh>
2025-04-28 14:57:41 +0000

    Update token authentication in middleware to retrieve token from Authorization header and ensure Bearer prefix is stripped before verification.

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 function authenticateToken(req, res, next) {
-  const token = req.cookies.token;
+  const token = req.headers.authorization;
   if (!token) return res.sendStatus(401);
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user; // { id, email, role }
     next();
